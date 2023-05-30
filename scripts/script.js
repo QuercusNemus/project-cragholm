8b9410d33acf513f4d7a3cb8c8f3655cd9d0cb62
Eric Andresen <eric@andresen.codes>
2023-05-30 15:59:07 +0200

    Added: sticky menu.

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,21 @@ function toggleMenu() {
     let menu = document.getElementById(`header`)
     menu.classList.toggle("nav-open")
 }
+
+const sectionHeroEl = document.querySelector(".section-hero")
+
+const observer = new IntersectionObserver(function (entries) {
+    const ent = entries[0]
+    if (!ent.isIntersecting) {
+        document.body.classList.add("sticky")
+    }
+
+    if (ent.isIntersecting) {
+        document.body.classList.remove("sticky")
+    }
+}, {
+    root: null,
+    threshold: 0,
+    rootMargin: "-100px"
+})
+observer.observe(sectionHeroEl)
