59dd2f726322acf8e3e34340d4884c626fdf77af
Eric Andresen <eric@andresen.codes>
2023-06-02 09:26:25 +0200

    Created save function for the form.

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,15 +1,18 @@
 import {Form} from "../src/models/index.js";
-import {DataStore} from 'node_modules/@aws-amplify/datastore';
+import {DataStore} from '@aws-amplify/datastore';
 
-try {
-    const post = await DataStore.save(
-        new Form({
-            "full_name": "Eurig Oakshield",
-            "email": "eurig@oak.lett",
-            "heard_from": "Facebook"
-        })
-    )
-    console.log('Post saved successfully!', post);
-} catch (error) {
-    console.log('Error saving post', error);
+async function saveForm() {
+    try {
+        const post = await DataStore.save(
+            new Form({
+                "full_name": "Eurig Oakshield",
+                "email": "eurig@oak.lett",
+                "heard_from": "Facebook"
+            })
+        )
+        console.log('Post saved successfully!', post);
+    } catch (error) {
+        console.log('Error saving post', error);
+    }
+
 }
